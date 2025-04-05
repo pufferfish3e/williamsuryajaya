b21031d82fb4b406bd750d1ef78a37218c9e6780
Kendrick <137743645+pufferfish3e@users.noreply.github.com>
2025-04-05 14:54:59 +0000

    initial commit

--- williamsuryajaya/src/components/goback.tsx
+++
@@ -1,6 +0,0 @@
-import style from './goback.module.css';
-import {Link} from 'react-router-dom';
-
-export default function Goback() {
-    return <Link to='/'><button className={style.button}>Home</button></Link>
-}
--- williamsuryajaya/src/pages/notfound/notfound.tsx
+++
@@ -1,3 +0,0 @@
-export default function NotFound() {
-    return <h1>This page doesn't exist.</h1>
-}
